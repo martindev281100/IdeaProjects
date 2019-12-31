8552a0b71b98c1fa9705d15d9ccf84a4b761f133
Manh <43464766+martindevox2811@users.noreply.github.com>
2019-12-31 08:48:38 +0700

    31/12/2019

--- CodeDemo/src/com/fgw/gch0712/MessageSystem/Main.java
+++
@@ -1,8 +0,0 @@
-package com.fgw.gch0712.MessageSystem;
-
-public class Main {
-    public static void main(String[] args) {
-        MenuSystem a = new MenuSystem();
-        a.menu();
-    }
-}
--- CodeDemo/src/com/fgw/gch0712/MessageSystem/QueueEmptyException.java
+++
@@ -1,5 +0,0 @@
-package com.fgw.gch0712.MessageSystem;
-
-public class QueueEmptyException extends RuntimeException {
-    public QueueEmptyException(String messages){ super(); }
-}
